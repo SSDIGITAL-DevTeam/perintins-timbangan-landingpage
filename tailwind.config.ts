4287599fd9c6859d2465e67d33447415d626b07f
rinaldiihsan <rinaldi.210170168@mhs.unimal.ac.id>
2024-09-09 13:54:25 +0700

    init: first init app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,17 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
+      fontFamily: {
+        body: 'Lato, sans-serif',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: '#F6F6F6',
+        primary: '#D70700',
+        secondary: '#B10903',
+        tertiary: '#FFFAF1',
       },
     },
   },
