2dce9086c9b6250a5d9bff0921c2f995e12c9eb4
rinaldiihsan <rinaldi.210170168@mhs.unimal.ac.id>
2024-09-10 16:06:46 +0700

    fix: text footer

--- src/components/layouts/Footer/FooterUI.tsx
+++ src/components/layouts/Footer/FooterUI.tsx
@@ -8,11 +8,11 @@ import LogoWhite from '@/assets/svg/logo-white.svg';
 export default function FooterUI(): JSX.Element {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="bg-[#262626] py-6 px-16 flex items-center justify-between">
+    <footer className="bg-[#262626] py-6 px-16 flex flex-col lg:flex-row items-center justify-between gap-y-7 lg:gap-y-0">
       <Link href="/">
         <Image src={LogoWhite} alt="Logo Perintis Timbangan Indonesia" className="w-full h-full" />
       </Link>
-      <div className="text-right w-[20%]">
+      <div className="text-center lg:text-right lg:w-[20%]">
         <p className="text-white"> Copyright © {currentYear} PT Perintis Timbangan Indonesia Developed by OCTOBEES</p>
       </div>
     </footer>
