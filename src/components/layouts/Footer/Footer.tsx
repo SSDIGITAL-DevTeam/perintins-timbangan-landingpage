8240dc5c3e4ebcfca5ed4e6842653b79698b469a
ilhamdanu13 <ilham.danu13.id@gmail.com>
2024-09-13 09:49:38 +0700

    `feat:` add phone number link

--- src/components/layouts/Footer/Footer.tsx
+++ src/components/layouts/Footer/Footer.tsx
@@ -1,18 +1,22 @@
-'use client';
+"use client";
 
-import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
-import Logo from '@/assets/svg/logo.svg';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "@/assets/svg/logo.svg";
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   <footer className="bg-[#262626] p-6 flex items-center justify-between">
     <Link href="/">
-      <Image src={Logo} alt="Logo Perintis Timbangan Indonesia" className="w-full h-full" />
+      <Image
+        src={Logo}
+        alt="Logo Perintis Timbangan Indonesia"
+        className="w-full h-full"
+      />
     </Link>
-    <div className="text-right w-1/3">
-      <p> Copyright &copy {currentYear} PT Perintis Timbangan Indonesia Developed by OCTOBEES</p>
+    <div className="text-right ">
+      <p> Copyright &copy {currentYear} PT Perintis Timbangan Indonesia</p>
     </div>
   </footer>;
 }
--- src/components/layouts/Navbar/Navbar.tsx
+++ src/components/layouts/Navbar/Navbar.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
-import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
-import Logo from '@/assets/svg/logo.svg';
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "@/assets/svg/logo.svg";
 
 const Navbar: React.FC = () => {
   const [showButton, setShowButton] = useState<boolean>(false);
@@ -20,10 +20,10 @@ const Navbar: React.FC = () => {
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
@@ -32,27 +32,38 @@ const Navbar: React.FC = () => {
       <div
         className={`
         flex items-center transition-all duration-300 ease-in-out
-        ${isScrolled ? 'justify-between' : 'justify-center'}
+        ${isScrolled ? "justify-between" : "justify-center"}
       `}
       >
         <Link
           href="/"
           className={`
           transition-all duration-300 ease-in-out
-          ${isScrolled ? 'mr-auto' : 'mx-auto'}
+          ${isScrolled ? "mr-auto" : "mx-auto"}
         `}
         >
-          <Image src={Logo} alt="Logo Perintis Timbangan Indonesia" className="w-full h-full" />
+          <Image
+            src={Logo}
+            alt="Logo Perintis Timbangan Indonesia"
+            className="w-28 md:w-40 object-cover"
+          />
         </Link>
         <div
           className={`
           block md:hidden 
           fixed right-4 top-4
           transition-all duration-300 ease-in-out
-          ${showButton ? 'translate-y-4 opacity-100' : '-translate-y-full opacity-0'}
+          ${
+            showButton
+              ? "translate-y-4 opacity-100"
+              : "-translate-y-full opacity-0"
+          }
         `}
         >
-          <Link href="/kunjungi-toko" className="px-6 py-3 bg-primary text-white font-semibold hover:bg-secondary transition-colors duration-300 rounded-full">
+          <Link
+            href="/kunjungi-toko"
+            className="px-6 py-3 bg-primary text-white font-semibold hover:bg-secondary transition-colors duration-300 rounded-full"
+          >
             Kunjungi Toko Kami
           </Link>
         </div>
